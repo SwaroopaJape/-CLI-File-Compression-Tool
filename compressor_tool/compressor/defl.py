55d0370201e82a4ef18def654e5c17258519c346
SwaroopaJape <158248850+SwaroopaJape@users.noreply.github.com>
2025-12-06 23:39:34 +0530

    Refined comments

--- compressor_tool/compressor/defl.py
+++ compressor_tool/compressor/defl.py
@@ -1,17 +1,13 @@
 from .lz77 import compressor as lz_compressor, decompressor as lz_decompressor
 from .hce import compressor as hc_compressor, decompressor as hc_decompressor
 
+# function to compress data using deflation: first LZ77, then HCE
 def compressor(data):
     tokens = lz_compressor(data)
     return hc_compressor(tokens)
 
+# function to decompress data using deflation: first HCE, then LZ77
 def decompressor(data):
     tokens = hc_decompressor(data)
     return lz_decompressor(tokens)
 
-# test
-# A =  "Lorem ipsum dolor sit amet. Ut internos quia sed quam rerum sit suscipit maiores ea facere quod. Hic maxime maiores ut modi cupiditate ut ipsum voluptate. Aut autem numquam ut illum odit quo fugit necessitatibus. Qui facere magnam ut quod repellendus est debitis quibusdam qui cumque quia."
-# B = compressor(A.encode('latin-1'))
-# C = decompressor(B).decode('latin-1')
-
-# print(C==A)
